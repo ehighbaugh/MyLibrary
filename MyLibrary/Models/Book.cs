bf9b8ca00de6f605188a7ea6fe42c82448fdbeb9
Erin Highbaugh <erinhighbaugh@gmail.com>
2018-05-10 19:52:45 -0400

    Created MyLibrary app; initialized controller, view and model

---
+++ MyLibrary/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyLibrary.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public string Format { get; set; }
+        [Display(Name = "Year Published")]
+        public int? YearPublished { get; set; }
+    }
+
+    public class BookDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
